d8ac8dfd90bef736e0f41115dbefaedc8d31840a
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2017-10-27 13:47:08 +0200

    adjust scripts to push tags to Gerrit
    (cherry picked from commit 71103f39ab48b594a13f967bd5e05849fc62db49)

--- script/release/kie-pushTag.sh
+++ script/release/kie-pushTag.sh
@@ -6,8 +6,10 @@ echo "target" : $target
 # clone the build-bootstrap that contains the other build scripts
 if [ "$target" == "community" ]; then
    source=kiegroup
+   remote=origin
 else
    source=jboss-integration
+   remote=gerrit
 fi
 
 git clone git@github.com:"$source"/droolsjbpm-build-bootstrap.git --branch $releaseBranch --depth 100
@@ -15,9 +17,12 @@ git clone git@github.com:"$source"/droolsjbpm-build-bootstrap.git --branch $rele
 # clone rest of the repos and checkout to this branch
 ./droolsjbpm-build-bootstrap/script/git-clone-others.sh --branch $releaseBranch --depth 100
 
+# add a new remote pointing to gerrit
+./droolsjbpm-build-bootstrap/script/git-add-remote-gerrit.sh
+
 # create a tag
 commitMsg="Tagging $tag"
 ./droolsjbpm-build-bootstrap/script/git-all.sh tag -a $tag -m "$commitMsg"
 
 # pushes tag to the SOURCE
-./droolsjbpm-build-bootstrap/script/git-all.sh push origin $tag
+./droolsjbpm-build-bootstrap/script/git-all.sh push $remote $tag
