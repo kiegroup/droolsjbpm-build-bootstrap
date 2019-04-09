74cf23a4c052106cbe5793b1e496c5e46965791e
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-04-09 16:01:41 +0200

    refactored sript for auxiliary reps

--- script/release/kie-upgrade-branch-auxiliary-reps.sh
+++ script/release/kie-upgrade-branch-auxiliary-reps.sh
@@ -14,66 +14,139 @@ set -euxo pipefail
 #                                                                           *
 # ***************************************************************************
 
-# verify the number of changed files is greater checks how many files have changed after version upgrade
-verifyNumberOfUpdatedFiles () {
+countSNAPHOTS (){
    # count pom.xml files with SNAPSHOT version
    pomsWithSnapshotVersionCount=$(grep snapshot -rli --include=pom.xml --exclude-dir=src | wc -l)
-   # count number of files changed in git
+}
+
+countChanges (){
+      # count number of files changed in git
    changedFilesCount=$(git status -s | wc -l)
+}
+
+checkDiff (){
    if [ "$changedFilesCount" -gt "$pomsWithSnapshotVersionCount" ]; then
-     echo "Version upgrade of $repo to $newSnapshot failed."
-     echo "Expected number of changed poms ($changedFilesCount) to be <= number of poms containing SNAPSHOT version ($pomsWithSnapshotVersionCount)"
-     exit 1
+      echo "Version upgrade of $repo failed."
+      echo "Expected number of changed poms ($changedFilesCount) to be <= number of poms containing SNAPSHOT version ($pomsWithSnapshotVersionCount)"
+      exit 1
    fi
 }
 
-cloneRepoAndCheckoutBranches () {
-    git clone git@github.com:kiegroup/"$repo".git
-    cd "$repo"
-    git checkout -b "$newBranch" master
-    git checkout master
+cloneBranch (){
+   git clone git@github.com:kiegroup/"$repo".git
+   cd "$repo"
 }
 
-commitAndPushBranches () {
-    git add .
-    git commit -m "upgraded to new $newSnapshot version"
-    git push origin "$newBranch"
-    git push origin master
-    cd ..
+checkoutNewBranch (){
+   git checkout -b "$newBranch" master
+}
+
+commitBranch () {
+   git add .
+   git commit -m "upgraded to next SNAPSHOT"
+}
+
+pushBranch () {
+   git push origin master
+}
+
+pushBranches () {
+   git push origin master
+   git push origin "$newBranch"
+}
+
+updateVersion-kie-docker-ci-images () {
+  ./scripts/update-versions.sh "$newSNAPSHOT" -U
+}
+
+updateVersion-kie-benchmarks () {
+  # upgrade the versions to next SNAPSHOT
+  mvn -B -N -e versions:update-parent -Dfull -DparentVersion="[$newSNAPSHOT]" -DallowSnapshots=true -DgenerateBackupPoms=false
+  mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false
+  # do manual changes to pom files that were not changed in version upgrade
+  sed -i "s/<version>$oldSNAPSHOT<\/version>/<version>$newSNAPSHOT<\/version>/;P;D" jbpm-benchmarks/kieserver-assets/pom.xml
+  sed -i "s/$oldSNAPSHOT/$newSNAPSHOT/g" jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
+}
+
+updateVersion-kie-cloud-tests () {
+  # upgrade the versions to the next SNAPSHOT
+  mvn -B -N -e versions:update-parent -Dfull -DparentVersion="[$newSNAPSHOT]" -DallowSnapshots=true -DgenerateBackupPoms=false
+  mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false
 }
 
 # ******************** kie-docker-ci-images ************************************
 repo="kie-docker-ci-images"
-cloneRepoAndCheckoutBranches
 
-# upgrade the versions to next SNAPSHOT
-./scripts/update-versions.sh $newSnapshot -U
+if [ "$proc" == "oneBranch" ] ;then
+  cloneBranch
+  countSNAPHOTS
+  updateVersion-kie-docker-ci-images
+  countChanges
+  checkDiff
+  commitBranch
+  pushBranch
+else
+  cloneBranch
+  countSNAPHOTS
+  updateVersion-kie-docker-ci-images
+  countChanges
+  checkDiff
+  commitBranch
+  checkoutNewBranch
+  newSNAPSHOT=$newBranchSNAPSHOT
+  updateVersion-kie-docker-ci-images
+  countChanges
+  checkDiff
+  commitBranch
+  pushBranches
+fi
 
-verifyNumberOfUpdatedFiles
-commitAndPushBranches
 
 # ******************** kie-benchmarks ************************************
 
 repo="kie-benchmarks"
-cloneRepoAndCheckoutBranches
 
-# upgrade the versions to next SNAPSHOT
-mvn -B -N -e versions:update-parent -Dfull -DparentVersion="[$newSnapshot]" -DallowSnapshots=true -DgenerateBackupPoms=false
-mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false
-
-# do manual changes to pom files that were not changed in versin upgrade
-sed -i "s/<version>$oldSnapshot<\/version>/<version>$newSnapshot<\/version>/;P;D" jbpm-benchmarks/kieserver-assets/pom.xml
-
-verifyNumberOfUpdatedFiles
-commitAndPushBranches
+if [ "$proc" == "oneBranch" ] ;then
+  cloneBranch
+  updateVersion-kie-benchmarks
+  commitBranch
+  pushBranch
+else
+  cloneBranch
+  updateVersion-kie-benchmarks
+  commitBranch
+  checkoutNewBranch
+  oldSNAPSHOT=$newSNAPSHOT
+  newSNAPSHOT=$newBranchSNAPSHOT
+  updateVersion-kie-benchmarks
+  commitBranch
+  pushBranches
+fi
 
 # ********************* kie-cloud-tests ***********************************
+
 repo="kie-cloud-tests"
-cloneRepoAndCheckoutBranches
 
-# upgrade the versions to the next SNAPSHOT
-mvn -B -N -e versions:update-parent -Dfull -DparentVersion="[$newSnapshot]" -DallowSnapshots=true -DgenerateBackupPoms=false
-mvn -B -N -e versions:update-child-modules -Dfull -DallowSnapshots=true -DgenerateBackupPoms=false
-
-verifyNumberOfUpdatedFiles
-commitAndPushBranches
+if [ "$proc" == "oneBranch" ] ;then
+  cloneBranch
+  countSNAPHOTS
+  updateVersion-kie-cloud-tests
+  countChanges
+  checkDiff
+  commitBranch
+  pushBranch
+else
+  cloneBranch
+  countSNAPHOTS
+  updateVersion-kie-cloud-tests
+  countChanges
+  checkDiff
+  commitBranch
+  checkoutNewBranch
+  newSNAPSHOT=$newBranchSNAPSHOT
+  updateVersion-kie-cloud-tests
+  countChanges
+  checkDiff
+  commitBranch
+  pushBranches
+fi
