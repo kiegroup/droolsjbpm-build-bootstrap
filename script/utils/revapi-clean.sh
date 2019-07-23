43e9cf320decf8c863cc6f4b51096b2fd0f6ee52
Marián Macik <mmacik@redhat.com>
2019-07-23 10:28:45 +0200

    Further automate revapi cleaning

--- script/utils/revapi-clean.sh
+++ script/utils/revapi-clean.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# How to clean ignores in revapi-config.json files:
+#
+# export REVAPI_REPOSITORIES=droolsjbpm-build-bootstrap,droolsjbpm-knowledge,drools,jbpm,droolsjbpm-integration,optaplanner
+#
+# ./git-clone-others.sh --repo-list=$REVAPI_REPOSITORIES --add-upstream-remote
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES checkout master
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES pull --rebase upstream master
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES checkout -b revapi-7.23.0.Final
+# Copy this script to root of all repositories and run it there, ./revapi-clean.sh 7.23.0.Final
+# Update <revapi.oldKieVersion> in kie-parent
+# ./mvn-all.sh --repo-list=$REVAPI_REPOSITORIES clean install -DskipTests
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES add -u
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES commit -m "BAQE-1039 - Change revapi to check against 7.23.0.Final"
+# ./git-all.sh --repo-list=$REVAPI_REPOSITORIES push origin revapi-7.23.0.Final
+# Create PRs
 # Utility script for removing ignores in revapi-config.json files.
 
 usage ()
