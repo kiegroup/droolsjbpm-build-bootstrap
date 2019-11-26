9cf74644249ba3c6e791102cd97a3b89a7a8473a
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-11-26 14:57:55 +0100

    removed obsolte scripts

--- script/release/kie-removeReleaseBranchesGerrit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# removes release branches on Gerrit since the tag is already on Gerrit
-if [ "$target" == "productized" ]; then
-  ./droolsjbpm-build-bootstrap/script/git-all.sh push origin :$releaseBranch
-fi
