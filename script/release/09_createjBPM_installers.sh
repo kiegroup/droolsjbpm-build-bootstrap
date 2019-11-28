a49bdf23e97bd1d9d4a374467349babc30b33ebd
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-11-28 09:40:01 +0100

    changed .kieVersion to .version

--- script/release/09_createjBPM_installers.sh
+++ script/release/09_createjBPM_installers.sh
@@ -13,13 +13,13 @@ createJbpmInstaller(){
         unzip jbpm-installer*.zip
 
         # modifications in build.properties
-        sed -i -e '/^#/!s/jBPM.kieVersion=\${snapshot.kieVersion}/jBPM.kieVersion=\${release.kieVersion}/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/jBPM.url=http.*/jBPM.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/drools\/droolsjbpm-bpms-distribution\/\${jBPM.kieVersion}\/droolsjbpm-bpms-distribution-\${jBPM.kieVersion}-bin.zip/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/jBPM.console.url=http.*/jBPM.console.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/kie\/business-central\/\${jBPM.kieVersion}\/business-central-\${jBPM.kieVersion}-wildfly14.war/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/jBPM.casemgmt.url=https.*/jBPM.casemgmt.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/jbpm\/jbpm-wb-case-mgmt-showcase\/\${jBPM.kieVersion}\/jbpm-wb-case-mgmt-showcase-\${jBPM.kieVersion}-wildfly14.war/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/kie.server.url=http.*/kie.server.url=http:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/kie\/server\/kie-server\/${jBPM.kieVersion}\/kie-server-\${jBPM.kieVersion}-ee7.war/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/droolsjbpm.eclipse.kieVersion=\${snapshot.kieVersion}/droolsjbpm.eclipse.kieVersion=\${release.kieVersion}/g' jbpm-installer-$kieVersion/build.properties
-        sed -i -e '/^#/!s/droolsjbpm.eclipse.url=http.*/droolsjbpm.eclipse.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/drools\/org.drools.updatesite\/\${droolsjbpm.eclipse.kieVersion}\/org.drools.updatesite-\${droolsjbpm.eclipse.kieVersion}.zip/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/jBPM.version=\${snapshot.version}/jBPM.version=\${release.version}/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/jBPM.url=http.*/jBPM.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/drools\/droolsjbpm-bpms-distribution\/\${jBPM.version}\/droolsjbpm-bpms-distribution-\${jBPM.version}-bin.zip/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/jBPM.console.url=http.*/jBPM.console.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/kie\/business-central\/\${jBPM.version}\/business-central-\${jBPM.version}-wildfly14.war/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/jBPM.casemgmt.url=https.*/jBPM.casemgmt.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/jbpm\/jbpm-wb-case-mgmt-showcase\/\${jBPM.version}\/jbpm-wb-case-mgmt-showcase-\${jBPM.version}-wildfly14.war/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/kie.server.url=http.*/kie.server.url=http:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/kie\/server\/kie-server\/${jBPM.version}\/kie-server-\${jBPM.version}-ee7.war/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/droolsjbpm.eclipse.version=\${snapshot.version}/droolsjbpm.eclipse.version=\${release.version}/g' jbpm-installer-$kieVersion/build.properties
+        sed -i -e '/^#/!s/droolsjbpm.eclipse.url=http.*/droolsjbpm.eclipse.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/'$URLgroup'\/org\/drools\/org.drools.updatesite\/\${droolsjbpm.eclipse.version}\/org.drools.updatesite-\${droolsjbpm.eclipse.version}.zip/g' jbpm-installer-$kieVersion/build.properties
 
         # add modified build.properties to jbpm-installer-<version>.zip
         zip -ur jbpm-installer-$kieVersion.zip jbpm-installer-$kieVersion/build.properties
