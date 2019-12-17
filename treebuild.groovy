8502d30c69c169a085ae64182a97e932661e2ff7
Enrique Mingorance Cano <emingora@redhat.com>
2019-12-17 10:48:31 +0100

    BXMSPROD-592_2 treebuild.buildProject method error fixed

--- treebuild.groovy
+++ treebuild.groovy
@@ -1,25 +1,25 @@
 /**
  * Builds the downstream
- * @param projectLines a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  */
-def downstreamBuild(def projectLines) {
-    def lastLine = projectLines.get(projectLines.size() - 1)
+def downstreamBuild(def projectCollection) {
+    def lastLine = projectCollection.get(projectCollection.size() - 1)
 
     println "Downstream building ${lastLine} project"
-    upstreamBuild(projectLines, lastLine)
+    upstreamBuild(projectCollection, lastLine)
 }
 
 /**
  * Builds the upstream for an specific project
- * @param projectLines a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
+ * @param projectCollection a collection of items following the pattern PROJECT_GROUP/PROJECT_NAME, for example kiegroup/drools
  * @param currentProject the project to build the stream from, like kiegroup/drools
  */
-def upstreamBuild(def projectLines, String currentProject) {
+def upstreamBuild(def projectCollection, String currentProject) {
     println "Upstream building ${currentProject} project"
 
     // Build project tree from currentProject node
-    for (i = 0; currentProject != projectLines.get(i); i++) {
-        buildProject(projectLines.get(i))
+    for (i = 0; currentProject != projectCollection.get(i); i++) {
+        buildProject(projectCollection.get(i))
     }
 
     buildProject(currentProject)
@@ -38,7 +38,7 @@ def buildProject(String project) {
     sh "cd ${projectGroup}_${projectName}"
     githubscm.checkoutIfExists(projectName, "$CHANGE_AUTHOR", "$CHANGE_BRANCH", projectGroup, "$CHANGE_TARGET")
     maven.runMavenWithSubmarineSettings('clean install', true)
-    sh ".."
+    sh "cd .."
 }
 
 /**
