c8a7e991f72789e4b7f8c33e1918193a3675f8ce
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2020-01-08 09:36:42 +0100

    added script for erasing part of maven repository
    removed superfluous MAVEN_REPOS_LOCAL removal from scripts

--- script/release/05b_prodInstall.sh
+++ script/release/05b_prodInstall.sh
@@ -1,18 +1,4 @@
 #!/bin/bash -e
 
-# removing KIE artifacts from local maven repo (basically all possible SNAPSHOTs)
-echo $MAVEN_REPO_LOCAL "="
-
-if [ -d $MAVEN_REPO_LOCAL ]; then
-    rm -rf $MAVEN_REPO_LOCAL/org/jboss/dashboard-builder/
-    rm -rf $MAVEN_REPO_LOCAL/org/kie/
-    rm -rf $MAVEN_REPO_LOCAL/org/drools/
-    rm -rf $MAVEN_REPO_LOCAL/org/jbpm/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaplanner/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaweb
-    rm -rf $MAVEN_REPO_LOCAL/org/uberfire/
-
-fi
-
 # does a clean install
 ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dfull -Dproductized -Drelease -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g"
