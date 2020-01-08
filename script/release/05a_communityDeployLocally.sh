7598ed90998b7ee15138f08431ce43ab698f5d6d
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2020-01-08 14:18:59 +0100

    Install VS deploy (#1153)
    * prod tag doesn't need a deploy, clean install is OK
    
    * added script for erasing part of maven repository
    
    removed superfluous MAVEN_REPOS_LOCAL removal from scripts

--- script/release/05a_communityDeployLocally.sh
+++ script/release/05a_communityDeployLocally.sh
@@ -1,19 +1,5 @@
 #!/bin/bash -e
 
-# removing KIE artifacts from local maven repo (basically all possible SNAPSHOTs)
-echo $MAVEN_REPO_LOCAL "="
-
-if [ -d $MAVEN_REPO_LOCAL ]; then
-    rm -rf $MAVEN_REPO_LOCAL/org/jboss/dashboard-builder/
-    rm -rf $MAVEN_REPO_LOCAL/org/kie/
-    rm -rf $MAVEN_REPO_LOCAL/org/drools/
-    rm -rf $MAVEN_REPO_LOCAL/org/jbpm/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaplanner/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaweb
-    rm -rf $MAVEN_REPO_LOCAL/org/uberfire/
-
-fi
-
 deployDir=$WORKSPACE/community-deploy-dir
 # does a full build, but deploys only into local dir
 # we will deploy into remote staging repo only once the whole build passed (to save time and bandwith)
--- script/release/05d_dailyBuildProdInstall.sh
+++ script/release/05d_dailyBuildProdInstall.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-# removing KIE artifacts from local maven repo (basically all possible SNAPSHOTs)
-echo $MAVEN_REPO_LOCAL "="
-
-if [ -d $MAVEN_REPO_LOCAL ]; then
-    rm -rf $MAVEN_REPO_LOCAL/org/jboss/dashboard-builder/
-    rm -rf $MAVEN_REPO_LOCAL/org/kie/
-    rm -rf $MAVEN_REPO_LOCAL/org/drools/
-    rm -rf $MAVEN_REPO_LOCAL/org/jbpm/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaplanner/
-    rm -rf $MAVEN_REPO_LOCAL/org/optaweb
-    rm -rf $MAVEN_REPO_LOCAL/org/uberfire/
-fi
-
 # do a full build
 ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -Dfull -Drelease -Dproductized -s $SETTINGS_XML_FILE\
  -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true\
