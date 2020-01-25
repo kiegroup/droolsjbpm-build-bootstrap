20cd12ab8a62f9a775a05e06765cb310d4f5bc99
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2020-01-25 09:29:34 +0100

    fixed path

--- script/release/prepareUploadDir.sh
+++ script/release/prepareUploadDir.sh
@@ -3,7 +3,7 @@
 # fetch the <version.org.kie> from kie-parent-metadata pom.xml and set it on parameter KIE_VERSION
 kieVersion=$(sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -n -e 's/<version.org.kie>\(.*\)<\/version.org.kie>/\1/p' droolsjbpm-build-bootstrap/pom.xml)
 echo "kieVersion = "$kieVersion
-deployDir=$1/community-deploy-dir
+deployDir=../community-deploy-dir
 
 mkdir ${kieVersion}_uploadBinaries
 cd ${kieVersion}_uploadBinaries
