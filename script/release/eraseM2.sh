2be0314b81e3f5fc59272dcbba51113d9858c880
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2020-02-05 14:40:57 +0100

    upgraded m2 path for dashbuilder dependencies

--- script/release/eraseM2.sh
+++ script/release/eraseM2.sh
@@ -4,7 +4,7 @@
 # parameters: $1 = m2Dir (path to local maven repository)
 
 if [ -d $1 ]; then
-    rm -rf $1/org/jboss/dashboard-builder/
+    rm -rf $1/org/dashbuilder/
     rm -rf $1/org/kie/
     rm -rf $1/org/drools/
     rm -rf $1/org/jbpm/
