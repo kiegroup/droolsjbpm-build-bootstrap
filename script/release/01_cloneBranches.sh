7e35987c2fedf366acd3c39a7885be5ff7657440
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2020-07-07 18:21:08 +0200

    Jenkins file sync (#1388)
    * staging repo doesn't be removed automatically when upload fails (#1377)
    
    
    (cherry picked from commit 40d031a240503f1cd99044e8bc701edad24e65fa)
    
    * BXMSPROD-858: fix that clone script doesn't clone with --single-branch (default value unless --no-single-branch is specified) (#1386)
    
    
    (cherry picked from commit 00a013af30c16341b4f39773083b69db5a4c1bde)
    
    * fixed create release branches script not to depend on baseBranch parameter (#1387)
    
    
    (cherry picked from commit 724d52fca15de61c4bfd5acb53eb713613d28bc4)
    
    * synced Jenkinsfile like on master

--- script/release/01_cloneBranches.sh
+++ script/release/01_cloneBranches.sh
@@ -4,4 +4,4 @@
 # parameter: baseBranch = $1
 
 # clone all repos except droolsjbpm-build-bootstrap as this is supposed to be cloned before this script and has to be available
-./droolsjbpm-build-bootstrap/script/git-clone-others.sh --branch $1 --depth 70
+./droolsjbpm-build-bootstrap/script/git-clone-others.sh --branch $1 --depth 70 --no-single-branch
