6517bed168792855fb651aa62f4ba69a1a3a5b4e
Marián Macik <mmacik@redhat.com>
2020-07-27 15:09:23 +0200

    Fix env vars in revapi-clean.sh (#1410)

--- script/utils/revapi-clean.sh
+++ script/utils/revapi-clean.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # How to clean ignores in revapi-config.json files:
 #
-# export MASTER_REVAPI_REPOSITORIES=droolsjbpm-build-bootstrap,droolsjbpm-knowledge,drools,jbpm,droolsjbpm-integration
+# export MASTER_REVAPI_REPOSITORIES=droolsjbpm-build-bootstrap,appformer,droolsjbpm-knowledge,drools,jbpm,droolsjbpm-integration
 # export BRANCHED_7_REVAPI_REPOSITORIES=optaplanner
 # export REVAPI_REPOSITORIES=$MASTER_REVAPI_REPOSITORIES,$BRANCHED_7_REVAPI_REPOSITORIES
 #
 # ./git-clone-others.sh --repo-list=$REVAPI_REPOSITORIES --add-upstream-remote
-# ./git-all.sh --repo-list=MASTER_REVAPI_REPOSITORIES checkout master
-# ./git-all.sh --repo-list=MASTER_REVAPI_REPOSITORIES pull --rebase upstream master
-# ./git-all.sh --repo-list=BRANCHED_7_REVAPI_REPOSITORIES checkout 7.x
-# ./git-all.sh --repo-list=BRANCHED_7_REVAPI_REPOSITORIES pull --rebase upstream 7.x
+# ./git-all.sh --repo-list=$MASTER_REVAPI_REPOSITORIES checkout master
+# ./git-all.sh --repo-list=$MASTER_REVAPI_REPOSITORIES pull --rebase upstream master
+# ./git-all.sh --repo-list=$BRANCHED_7_REVAPI_REPOSITORIES checkout 7.x
+# ./git-all.sh --repo-list=$BRANCHED_7_REVAPI_REPOSITORIES pull --rebase upstream 7.x
 # ./git-all.sh --repo-list=$REVAPI_REPOSITORIES checkout -b revapi-7.23.0.Final
 # Copy this script to root of all repositories and run it there, ./revapi-clean.sh 7.23.0.Final
 # Update <revapi.oldKieVersion> in kie-parent
-# ./mvn-all.sh --repo-list=$REVAPI_REPOSITORIES clean install -DskipTests -Dskip.npm
+# ./mvn-all.sh --repo-list=$REVAPI_REPOSITORIES clean install -DskipTests -Dskip.npm -Dskip.yarn -Dgwt.compiler.skip
 # ./git-all.sh --repo-list=$REVAPI_REPOSITORIES add -u
 # ./git-all.sh --repo-list=$REVAPI_REPOSITORIES commit -m "BAQE-1039 - Change revapi to check against 7.23.0.Final"
 # ./git-all.sh --repo-list=$REVAPI_REPOSITORIES push origin revapi-7.23.0.Final
