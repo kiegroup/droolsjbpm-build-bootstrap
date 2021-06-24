14caca75f51ac80954415c4d68037fa55ebe41c4
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-06-24 16:29:32 +0200

    skipping tests to re-generate that artifacts to upload to filemgmt.jboos.org

--- script/release/05a_communityDeployLocally.sh
+++ script/release/05a_communityDeployLocally.sh
@@ -3,5 +3,5 @@
 deployDir=$WORKSPACE/community-deploy-dir
 # does a full build, but deploys only into local dir
 # we will deploy into remote staging repo only once the whole build passed (to save time and bandwith)
-./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dfull -Drelease -DaltDeploymentRepository=local::default::file://$deployDir -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g"
+./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dfull -Drelease -DaltDeploymentRepository=local::default::file://$deployDir -DskipTests -Dgwt.memory.settings="-Xmx10g"
 
