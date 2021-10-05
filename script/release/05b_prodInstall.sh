02161989743ba99d8ad472afb167cb04c7c4e60c
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-10-05 16:34:47 +0200

    added missing profiles to mvn commands (#1788)

--- script/release/05b_prodInstall.sh
+++ script/release/05b_prodInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # does a clean install
-./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dfull -Dproductized -Drelease -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g"
+./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dfull -Dproductized -Drelease -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g" -Prun-code-coverage  -Dcontainer.profile=wildfly -Dcontainer=wildfly -Dintegration-tests=true -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 -Dmaven.wagon.http.retryHandler.count=3
--- script/release/05c_dailyBuildDeployLocally.sh
+++ script/release/05c_dailyBuildDeployLocally.sh
@@ -4,6 +4,4 @@
 deployDir=$WORKSPACE/deploy-dir
 
 # do a full build, but deploy only into local dir
-./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Drelease -DaltDeploymentRepository=local::default::file://$deployDir -s $SETTINGS_XML_FILE\
- -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true\
- -Dgwt.memory.settings="-Xmx10g" --clean-up-script="$WORKSPACE/clean-up.sh"
+./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Drelease -DaltDeploymentRepository=local::default::file://$deployDir -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g" -Prun-code-coverage  -Dcontainer.profile=wildfly -Dcontainer=wildfly -Dintegration-tests=true -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 -Dmaven.wagon.http.retryHandler.count=3 --clean-up-script="$WORKSPACE/clean-up.sh"
