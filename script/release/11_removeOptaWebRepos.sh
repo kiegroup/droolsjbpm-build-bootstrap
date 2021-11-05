ee7319613027e6e084e8bc60299a2cbdff4e0f37
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2021-11-05 16:28:59 +0100

    RHDM-1840: fixed optaweb* removal script (#1814)

--- script/release/11_removeOptaWebRepos.sh
+++ script/release/11_removeOptaWebRepos.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
-sed 's/optaweb-vehicle-routing//g' droolsjbpm-build-bootstrap/script/repository-list.txt
-sed 's/optaweb-employee-rostering//g' droolsjbpm-build-bootstrap/script/repository-list.txt
+sed -i '/optaweb-employee-rostering/d' droolsjbpm-build-bootstrap/script/repository-list.txt
+sed -i '/optaweb-vehicle-routing/d' droolsjbpm-build-bootstrap/script/repository-list.txt
+sed -i '/optaweb-employee-rostering/d' droolsjbpm-build-bootstrap/script/branched-7-repository-list.txt
+sed -i '/optaweb-vehicle-routing/d' droolsjbpm-build-bootstrap/script/branched-7-repository-list.txt
+
+
