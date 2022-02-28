23132d23d1ea3dfeac0596e51b51e42596e5cef4
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2022-02-28 09:48:36 +0100

    removed obsolete scripts

--- script/release/05c_dailyBuildDeployLocally.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# build the repos & deploy into local dir
-deployDir=$WORKSPACE/deploy-dir
-
-# do a full build, but deploy only into local dir
-./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Drelease -DaltDeploymentRepository=local::default::file://$deployDir -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g" -Prun-code-coverage  -Dcontainer.profile=wildfly -Dcontainer=wildfly -Dintegration-tests=true -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 -Dmaven.wagon.http.retryHandler.count=3 --clean-up-script="$WORKSPACE/clean-up.sh"
--- script/release/05d_dailyBuildProdInstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-# do a full build
-./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean install -Dfull -Drelease -Dproductized -s $SETTINGS_XML_FILE -Dkie.maven.settings.custom=$SETTINGS_XML_FILE -Dmaven.test.redirectTestOutputToFile=true -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx10g" -Prun-code-coverage  -Dcontainer.profile=wildfly -Dcontainer=wildfly -Dintegration-tests=true -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 -Dmaven.wagon.http.retryHandler.count=3 --clean-up-script="$WORKSPACE/clean-up.sh"
