bd7b8032e39b4f36202eae313f3d3c0736b0e5de
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2022-02-28 09:53:26 +0100

    [WF] added Jenkinsfile and yaml file for daily builds (#1884)
    * [WF] added Jenkinsfile and yaml file for daily builds
    
    * removed obsolete scripts

--- script/release/02_createReleaseBranches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# checkout of cloned repositories to release branches
-# parameters: releaseBranch = $1
-
-
-./droolsjbpm-build-bootstrap/script/git-all.sh checkout -b $1
--- script/release/07_addRemoteToGerrit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# add remote to Gerrit for prod tags
-# this step is not needed for releases nor dailyBuilds
-
-./droolsjbpm-build-bootstrap/script/git-add-remote-gerrit.sh
-
