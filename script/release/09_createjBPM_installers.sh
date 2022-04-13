b2823ab2a50477aaa40207328759b647a6b500f4
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2022-04-13 12:43:58 +0200

    removed when condition
    fixed archived artifact name
    
    changes due to filemgmt servers

--- script/release/09_createjBPM_installers.sh
+++ script/release/09_createjBPM_installers.sh
@@ -5,7 +5,7 @@
 toolsVer=$1
 
 # fetch the <version.org.kie> from kie-parent-metadata pom.xml and set it on parameter KIE_VERSION
-kieVersion=$(sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -n -e 's/<version.org.kie>\(.*\)<\/version.org.kie>/\1/p' droolsjbpm-build-bootstrap/pom.xml)
+kieVersion=$(sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -n -e 's/<version.org.kie>\(.*\)<\/version.org.kie>/\1/p' bc/kiegroup_droolsjbpm_build_bootstrap/pom.xml)
 
 URLgroup="public-jboss"
 
