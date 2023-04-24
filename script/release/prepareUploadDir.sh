05b55b36b1b2c39940c1a1fee5e0a48294ff620f
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2023-04-24 14:57:16 +0200

    optaplanner and drools 7.x binaries are not any more needed to upload to the web (#2277)
    optaplanner and drools excluded from upload
    
    upgrade nodejs version

--- script/release/prepareUploadDir.sh
+++ script/release/prepareUploadDir.sh
@@ -8,12 +8,6 @@ deployDir=../community-deploy-dir
 mkdir ${kieVersion}_uploadBinaries
 cd ${kieVersion}_uploadBinaries
 
-# docs
-mkdir drools-docs
-cp  $deployDir/org/drools/drools-docs/$kieVersion/drools-docs-$kieVersion.zip drools-docs/
-unzip drools-docs/drools-docs-$kieVersion.zip -d drools-docs/
-rm drools-docs/drools-docs-$kieVersion.zip
-
 mkdir kie-api-javadoc
 scp  $deployDir/org/kie/kie-api/$kieVersion/kie-api-$kieVersion-javadoc.jar kie-api-javadoc/
 unzip kie-api-javadoc/kie-api-$kieVersion-javadoc.jar -d kie-api-javadoc/
@@ -24,29 +18,11 @@ cp  $deployDir/org/jbpm/jbpm-docs/$kieVersion/jbpm-docs-$kieVersion.zip jbpm-doc
 unzip jbpm-docs/jbpm-docs-$kieVersion.zip -d jbpm-docs/
 rm jbpm-docs/jbpm-docs-$kieVersion.zip
 
-mkdir optaplanner-docs
-cp  $deployDir/org/optaplanner/optaplanner-docs/$kieVersion/optaplanner-docs-$kieVersion.zip optaplanner-docs/
-unzip optaplanner-docs/optaplanner-docs-$kieVersion.zip -d optaplanner-docs/
-rm optaplanner-docs/optaplanner-docs-$kieVersion.zip
-
-# drools
-cp $deployDir/org/drools/drools-distribution/$kieVersion/drools-distribution-$kieVersion.zip .
-cp $deployDir/org/drools/droolsjbpm-integration-distribution/$kieVersion/droolsjbpm-integration-distribution-$kieVersion.zip .
-cp $deployDir/org/kie/business-central/$kieVersion/business-central-$kieVersion-*.war .
-cp $deployDir/org/kie/server/kie-server-distribution/$kieVersion/kie-server-distribution-$kieVersion.zip .
-
 # jbpm
 cp $deployDir/org/jbpm/jbpm-distribution/$kieVersion/jbpm-distribution-$kieVersion-bin.zip jbpm-$kieVersion-bin.zip
 cp $deployDir/org/jbpm/jbpm-distribution/$kieVersion/jbpm-distribution-$kieVersion-examples.zip jbpm-$kieVersion-examples.zip
 cp $deployDir/org/kie/jbpm-server-distribution/$kieVersion/jbpm-server-distribution-$kieVersion-dist.zip jbpm-server-$kieVersion-dist.zip
 
-# optaplanner
-cp $deployDir/org/optaplanner/optaplanner-distribution/$kieVersion/optaplanner-distribution-$kieVersion.zip .
-
 # copies binaries + docs that are only available in /target directories - they are not deployed
 mkdir service-repository
 cp -r ../bc/kiegroup_jbpm_work_items/repository/target/repository-$kieVersion/* service-repository
-mkdir optaplanner-javadoc
-cp -r ../bc/kiegroup_optaplanner/optaplanner-distribution/target/optaplanner-distribution-$kieVersion/optaplanner-distribution-$kieVersion/javadocs/* optaplanner-javadoc
-mkdir optaplanner-wb-es-docs
-cp -r ../bc/kiegroup_kie_docs/doc-content/optaplanner-wb-es-docs/target/generated-docs/* optaplanner-wb-es-docs
